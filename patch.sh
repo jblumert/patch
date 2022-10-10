8874dad1b9fa7a7c36bd4124906188e66f8da246
jblumert <66739278+jblumert@users.noreply.github.com>
2022-10-10 11:49:13 -0400

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,6 +3,7 @@ echo "MINIO_VOLUMES=/scale/fs1/minio/data" > /etc/default/minio
 sed -i 's/minio-user/root/' /etc/systemd/system/minio.service
 systemctl daemon-reload
 sleep 3
+systemctl enable minio
 systemctl start minio
 sleep 3
 mc alias set minio http://192.168.252.5:9000 minioadmin minioadmin
