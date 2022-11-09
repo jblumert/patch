ac10ab2af54ef5dc665511778813e06554f4400c
jblumert <66739278+jblumert@users.noreply.github.com>
2022-11-09 16:55:29 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# patch for minio
+#
 echo "MINIO_VOLUMES=/scale/fs1/minio/data" > /etc/default/minio
 sed -i 's/minio-user/root/' /etc/systemd/system/minio.service
 systemctl daemon-reload
@@ -8,3 +10,10 @@ systemctl start minio
 sleep 3
 mc alias set minio http://192.168.252.5:9000 minioadmin minioadmin
 mc mb minio/backup
+#
+# scale patch
+# rpm -ivh /usr/lpp/mmfs/5.1.5.0/zimon_rpms/rhel8/gpfs.gss.pmsensors-5.1.5-0.el8.x86_64.rpm
+# systemctl start pmsensors
+# systemctl restart pmcollector
+# systemctl restart gpfsgui
+# mmsysmoncontrol restart
